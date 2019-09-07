06615aa4ee32e6146bee1039c7ac10ca508f49b4
Stdio <kwork-stdio@mail.ru>
2019-09-07 15:44:28 +0300

    0.0.1

--- app/src/main/java/com/stdio/imagepicker/MainActivity.java
+++ app/src/main/java/com/stdio/imagepicker/MainActivity.java
@@ -2,13 +2,42 @@ package com.stdio.imagepicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivPassportPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivPassportPhoto = findViewById(R.id.ivPassportPhoto);
+    }
+
+    public void chooseImage(View view) {
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+        startActivityForResult(intent, 1213);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1213 && resultCode == Activity.RESULT_OK) {
+            String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+            Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+            ivPassportPhoto.setImageBitmap(selectedImage);
+        }
     }
 }
